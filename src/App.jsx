eae2144e3604789ade14224067566acfecfb119d
Sudharsanan-coder <sudharsanantv2005@gmail.com>
2025-05-08 20:32:01 +0530

    suma

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   return (
   <div className='overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900'>
   
-  <div className=''></div>
+  <div className='bg-black'></div>
   
   
   
