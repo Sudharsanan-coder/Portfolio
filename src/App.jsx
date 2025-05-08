b4a4276dcac5e7253b3a3b3e50b6ea02c2bb69e4
Sudharsanan-coder <sudharsanantv2005@gmail.com>
2025-05-08 20:34:18 +0530

    as

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,9 @@ import Certificate from './Components/Certificates/Certificate.jsx'
 function App() {
   
   return (
-  <div className='overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900'>
+  <div className='overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900 bg-black'>
   
-  <div className='bg-black'></div>
+  
   
   
   
