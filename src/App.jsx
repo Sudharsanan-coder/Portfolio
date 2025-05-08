1ee1ac0d83d4801ed49b200a88b5eb1da63c6b6b
Sudharsanan-coder <sudharsanantv2005@gmail.com>
2025-05-08 21:10:37 +0530

    as

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   
   return (
   <div className='overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900'>
-  <StarryNightSky>
+  <StarryNightSky></StarryNightSky>
   
   
   
@@ -27,11 +27,8 @@ function App() {
     <Certificate/>
     <Email/>
     <Contact/>
-
-    
     
   </div>
-  </StarryNightSky>
   </div>
   
   )
