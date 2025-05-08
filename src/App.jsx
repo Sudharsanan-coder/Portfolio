d8b409f747f54a209a2c7f7f38f25b0d9ae7feb7
Sudharsanan-coder <sudharsanantv2005@gmail.com>
2025-05-08 21:55:13 +0530

    suma

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,15 @@ import Contact from './Components/Contact.jsx'
 import Email from './Components/Email.jsx'
 import Certificate from './Components/Certificates/Certificate.jsx'
 import StarryNightSky from './Components/StarryNightSky.jsx'
-
+import back from '../public/Background.png'
 function App() {
   
   return (
   <div className='overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900 '>
-    <StarryNightSky></StarryNightSky>
+   <div 
+        className="fixed inset-0 -z-10 bg-cover bg-center bg-no-repeat bg-fixed"
+        style={{ backgroundImage: "url('/Background.png')" }}
+      ></div>
   <div className=' container mx-auto'>
   
     <Navbar />
@@ -25,6 +28,7 @@ function App() {
     <Contact/>
     
   </div>
+  
   </div>
   
   )
