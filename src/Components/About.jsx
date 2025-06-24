e4ac634110435b7fc40b3b8135b36b0524298b3c
Sudharsanan <sudharsanantv2005@gmail.com>
2025-06-25 00:19:39 +0530

    any name

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -21,7 +21,9 @@ const About = () => {
           </motion.div>
           <motion.div whileInView={{opacity:1,x:0}} initial={{opacity:0,x:100}} transition={{duration:0.5}}
           className='w-full  lg:w-1/2 mt-8 text-lg'>
-                <p>zz</p>
+                <p>As a Full Stack Developer and UI/UX Designer, I specialize in creating end-to-end web solutions that combine functionality with intuitive design. I have a deep understanding of both front-end and back-end technologies, which enables me to build applications that not only look good but also perform exceptionally well. My approach centers around user experience, ensuring that every project I work on is visually engaging and easy to navigate.
+
+I am passionate about coding, problem-solving, and bringing creative ideas to life through technology. With a strong eye for design and a commitment to writing clean, maintainable code, I thrive in roles that challenge me to blend technical proficiency with creativity. I am always looking to stay ahead of industry trends and expand my skill set to deliver innovative and high-quality digital experiences.</p>
           </motion.div>
           
 
